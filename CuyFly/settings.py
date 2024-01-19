fc43bfed16d7b852478b401165cb72ff95f02d1e
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-01-19 13:50:00 -0500

    load images

--- CuyFly/settings.py
+++ CuyFly/settings.py
@@ -118,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+MEDIA_URL = '/imagenes/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/imagenes')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
